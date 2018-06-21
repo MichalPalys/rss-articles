07c9f9b11360451eeb4b87f8cf94b7ec4bffba78
Michal Palys <michael.palys@wp.pl>
2018-06-21 14:37:19 +0200

    usunięcie zbędnych linii kodu

--- src/Command/DisplayNeededDateCommand.php
+++ src/Command/DisplayNeededDateCommand.php
@@ -16,33 +16,13 @@
 
 namespace App\Command;
 
-//use App\Entity\Article;
-//use App\Repository\ArticleRepository;
-//use App\Service\ResponseCodeFromFeedService;
-//use PicoFeed\Reader\Reader;
-//use Psr\Log\LoggerInterface;
 use App\Service\FeedService;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-//use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DisplayNeededDateCommand extends ContainerAwareCommand
 {
-//    private $logger;
-//
-//    private $respCodeFromFeed;
-//
-//    private $articleRepository;
-//
-//    public function __construct(LoggerInterface $logger, ResponseCodeFromFeedService $respCodeFromFeed, ArticleRepository $articleRepository)
-//    {
-//        parent::__construct();
-//        $this->logger = $logger;
-//        $this->respCodeFromFeed = $respCodeFromFeed;
-//        $this->articleRepository = $articleRepository;
-//    }
-
     private $feedService;
 
     public function __construct(FeedService $feedService)
@@ -68,75 +48,6 @@ class DisplayNeededDateCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        $output->writeln($this->printRss());
         $output->writeln($this->feedService->setFeedToDataBase());
     }
-
-//    public function printRss()
-//    {
-//        // You can now use your logger
-//        $this->logger->info('Rozpoczęcie wykonywania skryptu.');
-//
-//        $rssLinkArray = [
-//            'http://www.rmf24.pl/sport/feed',
-//            'http://www.komputerswiat.pl/rss-feeds/komputer-swiat-feed.aspx',
-//            'http://xmoon.pl/rss/rss.xml',
-//        ];
-//
-//        foreach ($rssLinkArray as $rssLinkArrayValue) {
-//            try {
-//                $respCode = $this->respCodeFromFeed->getResponseCodeFromFeed($rssLinkArrayValue);
-//
-//                if ($respCode != 200) {
-//                    throw new Exception("HTTP Code = " . $respCode);
-//                }
-//
-//                $this->logger->info('Strona odpowiada. Kod odpowiedzi serwera: ' . $respCode . ' dla URL ' . $rssLinkArrayValue . "\n");
-//
-//                $reader = new Reader;
-//
-//                // Return a resource
-//                $resource = $reader->download($rssLinkArrayValue);
-//
-//                // Return the right parser instance according to the feed format
-//                $parser = $reader->getParser(
-//                    $resource->getUrl(),
-//                    $resource->getContent(),
-//                    $resource->getEncoding()
-//                );
-//
-//                // Return a Feed object
-//                $feed = $parser->execute();
-//
-//                foreach ($feed->items as $key => $val) {
-//                    $externalId = $feed->items[$key]->getId();
-//                    $itemArticleFlag = $this->articleRepository->findOneBy(['externalId' => $externalId]);
-//
-//                    if (!$itemArticleFlag) {
-//                        $article = new Article();
-//
-//                        //logowanie dodania pojedyńczego artykułu
-//                        $this->logger->info('Dodanie atrykułu z id: ' . $feed->items[$key]->getId());
-//
-//                        $article->setExternalId($feed->items[$key]->getId());
-//                        $article->setTitle($feed->items[$key]->getTitle());
-//                        $article->setPubDate($feed->items[$key]->getPublishedDate());
-//                        $article->setInsertDate($feed->items[$key]->getUpdatedDate());
-//                        $article->setContent($feed->items[$key]->getContent());
-//
-//                        // tell Doctrine you want to (eventually) save the $article (no queries yet)
-//                        $this->articleRepository->save($article);
-//                    }
-//                }
-//
-//                // actually executes the queries (i.e. the INSERT query)
-//                $this->articleRepository->execQuery();
-//            } catch (\Exception $e) {
-//                $this->logger->info('Kod błędu odpowiedzi serwera: ' . $respCode . ' dla URL ' . $rssLinkArrayValue . "\n");
-//            }
-//        }
-//
-//        // logowanie zakończenia skryptu
-//        $this->logger->info('Zakończenie wykonywania skryptu.');
-//    }
 }
