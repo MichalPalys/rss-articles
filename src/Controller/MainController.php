7f1bf33b35d74fa97e7fd97f0737104d013ec03b
Michal Palys <michael.palys@wp.pl>
2018-07-03 14:41:48 +0200

    utworzenie logiki biznesowej dla klasy ArticleModel

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Model\ArticleModel;
 use App\Repository\ArticleRepository;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
@@ -12,11 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends Controller
 {
-    private $articleRepository;
+//    private $articleRepository;
+//
+//    public function __construct(ArticleRepository $articleRepository)
+//    {
+//        $this->articleRepository = $articleRepository;
+//    }
+    private $articleModel;
 
-    public function __construct(ArticleRepository $articleRepository)
+    public function __construct(ArticleModel $articleModel)
     {
-        $this->articleRepository = $articleRepository;
+        $this->articleModel = $articleModel;
     }
 
     /**
@@ -26,16 +33,19 @@ class MainController extends Controller
     {
         $page = $request->query->get('page', 1);
 
-        $qb = $this->articleRepository->findAllQueryBuilder();
-        $adapter = new DoctrineORMAdapter($qb);
-        $pagerfanta = new Pagerfanta($adapter);
-        $pagerfanta
-            ->setMaxPerPage(10)
-            ->setCurrentPage($page);
+//        $qb = $this->articleRepository->findAllQueryBuilder();
+//        $adapter = new DoctrineORMAdapter($qb);
+//        $pagerfanta = new Pagerfanta($adapter);
+//        $pagerfanta
+//            ->setMaxPerPage(10)
+//            ->setCurrentPage($page);
+
+//        $articleModel = new ArticleModel();
+        $allArticles = $this->articleModel->displayAllArticles($page, 10);
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
-            'my_pager' => $pagerfanta,
+            'my_pager' => $allArticles,
         ]);
     }
 
@@ -44,7 +54,8 @@ class MainController extends Controller
      */
     public function displayOneArticles(string $slug, int $id)
     {
-        $singleArticle = $this->articleRepository->findOneBy(['slug' => $slug, 'id' => $id]);
+//        $singleArticle = $this->articleRepository->findOneBy(['slug' => $slug, 'id' => $id]);
+        $singleArticle = $this->articleModel->displayOneArticles($slug, $id);
 
         return $this->render('main/article.html.twig', [
             'singleArticle' => $singleArticle,
