7202b0d880c99b47b3fb0b6c90fede33be808db1
Michal Palys <michael.palys@wp.pl>
2018-07-16 09:45:06 +0200

    uzucie cs-fixera

--- src/Form/MyRegistrationFormType.php
+++ src/Form/MyRegistrationFormType.php
@@ -3,25 +3,25 @@
 namespace App\Form;
 
 use App\Entity\User;
+use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
+use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
-use EWZ\Bundle\RecaptchaBundle\Form\Type\EWZRecaptchaType;
-use EWZ\Bundle\RecaptchaBundle\Validator\Constraints\IsTrue as RecaptchaTrue;
 
 class MyRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('statuteCheckbox', CheckboxType::class, ['mapped'=>false, 'constraints'=>[
+            ->add('statuteCheckbox', CheckboxType::class, ['mapped' => false, 'constraints' => [
                 new IsTrue([
-                    'message'=>'Nie zaakceptowałeś Regulaminu!'
+                    'message' => 'Nie zaakceptowałeś Regulaminu!'
                 ])
             ]])
-            ->add('captcha', EWZRecaptchaType::class, ['mapped'=>false, 'constraints'=>[
+            ->add('captcha', EWZRecaptchaType::class, ['mapped' => false, 'constraints' => [
                 new RecaptchaTrue()
             ]])
         ;
