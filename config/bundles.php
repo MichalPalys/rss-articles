edf1ba76f177159a281521bb4e9f7449a79a4c16
Michal Palys <michael.palys@wp.pl>
2018-07-20 08:35:19 +0200

    composer require oneup/uploader-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Oneup\UploaderBundle\OneupUploaderBundle::class => ['all' => true],
 ];
