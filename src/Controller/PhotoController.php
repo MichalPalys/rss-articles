906bcca7259ca368d8e92c077fe5c17bffa393df
Michal Palys <michael.palys@wp.pl>
2018-08-01 14:27:43 +0200

    dodanie czy plik został wybrany z dysku do uploadu przy edycji

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -5,13 +5,12 @@ namespace App\Controller;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Photo;
 use App\Repository\PhotoRepository;
-use App\Form\EditPhotoEntityFormType;
 use App\Service\DataPhotoService;
 use League\Flysystem\Filesystem;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use EasyCorp\Bundle\EasyAdminBundle\Form\Util\LegacyFormHelper;
+use EasyCorp\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
 
 class PhotoController extends BaseAdminController
 {
@@ -27,6 +26,62 @@ class PhotoController extends BaseAdminController
         $this->photoRepository = $photoRepository;
         $this->dataPhotoService = $dataPhotoService;
         $this->fileSystem = $filesystem;
+    }
+
+
+    protected function editAction()
+    {
+        $this->dispatch(EasyAdminEvents::PRE_EDIT);
+
+        $id = $this->request->query->get('id');
+        $easyadmin = $this->request->attributes->get('easyadmin');
+        $entity = $easyadmin['item'];
+
+        if ($this->request->isXmlHttpRequest() && $property = $this->request->query->get('property')) {
+            $newValue = 'true' === mb_strtolower($this->request->query->get('newValue'));
+            $fieldsMetadata = $this->entity['list']['fields'];
+
+            if (!isset($fieldsMetadata[$property]) || 'toggle' !== $fieldsMetadata[$property]['dataType']) {
+                throw new \RuntimeException(sprintf('The type of the "%s" property is not "toggle".', $property));
+            }
+
+            $this->updateEntityProperty($entity, $property, $newValue);
+
+            // cast to integer instead of string to avoid sending empty responses for 'false'
+            return new Response((int)$newValue);
+        }
+
+        $fields = $this->entity['edit']['fields'];
+
+        $editForm = $this->executeDynamicMethod('create<EntityName>EditForm', array($entity, $fields));
+        $deleteForm = $this->createDeleteForm($this->entity['name'], $id);
+
+        $flag = $this->request->files->get('photo');
+
+        if ($flag['pathFile']) {
+        $editForm->handleRequest($this->request);
+            if ($editForm->isSubmitted() && $editForm->isValid()) {
+                $this->dispatch(EasyAdminEvents::PRE_UPDATE, array('entity' => $entity));
+
+                $this->executeDynamicMethod('preUpdate<EntityName>Entity', array($entity, true));
+                $this->executeDynamicMethod('update<EntityName>Entity', array($entity));
+
+                $this->dispatch(EasyAdminEvents::POST_UPDATE, array('entity' => $entity));
+
+                return $this->redirectToReferrer();
+            }
+        }
+
+        $this->dispatch(EasyAdminEvents::POST_EDIT);
+
+        $parameters = array(
+            'form' => $editForm->createView(),
+            'entity_fields' => $fields,
+            'entity' => $entity,
+            'delete_form' => $deleteForm->createView(),
+        );
+
+        return $this->executeDynamicMethod('render<EntityName>Template', array('edit', $this->entity['templates']['edit'], $parameters));
     }
 
     protected function createEntityFormBuilder($entity, $view)
