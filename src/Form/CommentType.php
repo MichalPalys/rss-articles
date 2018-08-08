02ce39b58eda2f51811ba1125eb286f6e6975508
Michal Palys <michael.palys@wp.pl>
2018-08-08 13:09:30 +0200

    dodanie ckeditor do pola edycji dodania komentarza front-end

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,13 +8,15 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextareaType::class)
+            ->add('content', CKEditorType::class)
             ->add('submit', SubmitType::class)
         ;
     }
