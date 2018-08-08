9324002b553f4722a5be79f5a0810d69a247d276
Michal Palys <michael.palys@wp.pl>
2018-08-08 15:58:02 +0200

    przenisienie wywoływania nazwy labela do twiga

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 
 class CommentType extends AbstractType
@@ -16,7 +15,7 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', CKEditorType::class)
+            ->add('content', TextareaType::class)
             ->add('submit', SubmitType::class)
         ;
     }
